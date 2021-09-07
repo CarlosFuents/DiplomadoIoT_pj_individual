ce70946f7c5967874759fb91e9f9c020c38cf52a
Carlos Fuentes <carlosfu7@gmail.com>
2021-09-07 15:04:18 -0500

    nueva libreria para control de leds

---
+++ K32L2B31A_Project/iot_sdk/iot_sdk_peripherals/leds.c
@@ -0,0 +1,22 @@
+/*
+ * leds.c
+ *  CARLOS MARIO FUENTES
+ *  Created on: 6/09/2021
+ *      Author: CARLOS MARIO FUENTES
+ *      driver para controlar LEDs de tarjeta FRDM-K32L2B3
+ */
+
+#include "leds.h"
+#include "fsl_gpio.h"
+
+void encender_led_verde(){
+	//encender led verde
+	GPIO_PinWrite(GPIOD,5,0);
+}
+
+void apagar_led_verde(){
+	//apaga led verde
+	GPIO_PinWrite(GPIOD,5,1);
+}
+
+
---
+++ K32L2B31A_Project/iot_sdk/iot_sdk_peripherals/leds.h
@@ -0,0 +1,20 @@
+/*
+ * leds.h
+ *
+ *  Created on: 6/09/2021
+ *      Author: CARLOS MARIO FUENTES
+ *      driver para controlar LEDs de tarjeta FRDM-K32L2B3
+ */
+
+#ifndef IOT_SDK_PERIPHERALS_LEDS_H_
+#define IOT_SDK_PERIPHERALS_LEDS_H_
+
+/*enciende led verde de tarjeta FRDM-K32L2B3*/
+
+void encender_led_verde();
+
+/*apaga led verde de tarjeta FRDM-K32L2B3*/
+
+void apagar_led_verde();
+
+#endif /* IOT_SDK_PERIPHERALS_LEDS_H_ */
